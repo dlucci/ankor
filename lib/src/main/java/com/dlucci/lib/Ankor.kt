b03d7facb2ff86b7bd62afae076adc739063d673
Derril Lucci <derril.lucci@gmail.com>
2020-02-22 08:34:27 -0500

    Adding android to our library

--- lib/src/main/java/com/dlucci/lib/Ankor.kt
+++ lib/src/main/java/com/dlucci/lib/Ankor.kt
@@ -1,7 +1,15 @@
 package com.dlucci.lib
 
+import android.view.View
+
 open class Ankor {
 
     fun getName() = "Hello world!"
+
+    class Builder {
+        private lateinit var url : String
+
+        private lateinit var view : View
+    }
 
 }
